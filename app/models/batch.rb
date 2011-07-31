d5ba24032ad36aa3b0b06627e9058711c69c2a19
Catalyst <catalyst@rocket.(none)>
2011-07-31 06:24:26 +0400

    revert one more timeRevert "Revert "single table inheritance for orders""
    This reverts commit 42222be0616f8f50467ee2f1fb1c66e86ec5160e.

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,9 +1,6 @@
-class PostOrder < ActiveResource::Base
+class Batch < ActiveResource::Base
   self.site = Rails.application.config.extrapost_site
   self.user = 'clearsky'
   self.password = 'T7ilk20doZ'
   
-  def batch
-    Batch.find(self.batch_id)
-  end
 end
---
+++ db/migrate/20110730154328_add_type_and_external_order_id_to_orders.rb
@@ -0,0 +1,28 @@
+class AddTypeAndExternalOrderIdToOrders < ActiveRecord::Migration
+  def self.up
+    add_column :orders, :type, :string, :default => 'Order', :null => false
+    add_column :orders, :external_order_id, :string, :limit => 64, :default => nil
+    
+    Order.all.each do |order|
+      if order.axiomus_order
+        order.type = 'AxiomusOrder'
+        order.external_order_id = order.axiomus_order.auth
+        order.save
+        order.axiomus_order.destroy
+      elsif order.extra_post_order
+        order.type = 'ExtraPostOrder'
+        order.external_order_id = order.extra_post_order.post_order_id
+        order.save
+        order.extra_post_order.destroy
+      else
+        puts "Order for #{order.client} without external order. Deleting."
+        order.destroy
+      end
+    end
+  end
+
+  def self.down
+    remove_column :orders, :external_order_id
+    remove_column :orders, :type
+  end
+end
