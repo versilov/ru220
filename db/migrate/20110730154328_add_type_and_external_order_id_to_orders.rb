eb6d07d244f26b80c0c47ad820d3b03ac147abf1
Catalyst <catalyst@rocket.(none)>
2011-07-31 06:36:54 +0400

    commented logic in migration

--- db/migrate/20110730154328_add_type_and_external_order_id_to_orders.rb
+++ db/migrate/20110730154328_add_type_and_external_order_id_to_orders.rb
@@ -3,22 +3,22 @@ class AddTypeAndExternalOrderIdToOrders < ActiveRecord::Migration
     add_column :orders, :type, :string, :default => 'Order', :null => false
     add_column :orders, :external_order_id, :string, :limit => 64, :default => nil
     
-    Order.all.each do |order|
-      if order.axiomus_order
-        order.type = 'AxiomusOrder'
-        order.external_order_id = order.axiomus_order.auth
-        order.save
-        order.axiomus_order.destroy
-      elsif order.extra_post_order
-        order.type = 'ExtraPostOrder'
-        order.external_order_id = order.extra_post_order.post_order_id
-        order.save
-        order.extra_post_order.destroy
-      else
-        puts "Order for #{order.client} without external order. Deleting."
-        order.destroy
-      end
-    end
+#    Order.all.each do |order|
+#      if order.axiomus_order
+#        order.type = 'AxiomusOrder'
+#        order.external_order_id = order.axiomus_order.auth
+#        order.save
+#        order.axiomus_order.destroy
+#      elsif order.extra_post_order
+#        order.type = 'ExtraPostOrder'
+#        order.external_order_id = order.extra_post_order.post_order_id
+#        order.save
+#        order.extra_post_order.destroy
+#      else
+#        puts "Order for #{order.client} without external order. Deleting."
+#        order.destroy
+#      end
+#    end
   end
 
   def self.down
