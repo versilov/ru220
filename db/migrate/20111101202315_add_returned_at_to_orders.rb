60591856d4ecfeadd27b7a58517bfe29a2958877
Stanislav Versilov <stas.versilov@gmail.com>
2011-11-02 01:08:28 +0400

    returned_at migration simplified

--- db/migrate/20111101202315_add_returned_at_to_orders.rb
+++ db/migrate/20111101202315_add_returned_at_to_orders.rb
@@ -1,13 +1,6 @@
 class AddReturnedAtToOrders < ActiveRecord::Migration
   def self.up
     add_column :orders, :returned_at, :datetime
-    Return.all.each do |ret|
-      order = ret.order
-      if order
-        order.update_attribute(:returned_at, ret.created_at)
-        order.add_event :description => 'Возврат', :date => ret.created_at
-      end
-    end
   end
 
   def self.down
