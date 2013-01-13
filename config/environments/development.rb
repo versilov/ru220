277ffdd7245101c7acfaaab8c5cc22668eb741b5
Stas Versilov <stas.versilov@gmail.com>
2013-01-13 18:28:37 +0400

    Switched back to development version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ Ru220::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
   
-  config.extrapost_site = 'http://extrapost2.herokuapp.com'
+  config.extrapost_site = 'http://localhost:7000'
   
   # Axiomus delivery config
   config.axiomus_uid = 92
